e7ecd6c164b51918e668067ae915930d4a7229d1
Akeem Lawrence <akeemwlawrence@gmail.com>
2022-10-28 19:49:04 -0400

    This lab took me 13 minutes to solve

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "akeem",
+    streetAddress: "1992 Third Avenue",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const copy = {...employee};
+    copy[key] = value;
+    return copy;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    const copy = {...employee};
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const copy = {...employee};
+    copy[key] = value;
+    delete copy[key];
+    return copy;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    const copy = {...employee};
+    employee[key] = value;
+    return employee;
+}
