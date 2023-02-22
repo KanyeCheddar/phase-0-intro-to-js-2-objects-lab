1e321c4059d220cb141bab8cc99ea611d2959078
Akeem Lawrence <akeemwlawrence@gmail.com>
2023-02-22 16:37:11 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 // Write your solution in this file!
 const employee = {
-    name: "akeem",
-    streetAddress: "1992 Third Avenue",
+    name: 'akeem',
+    streetAddress: '1992 Third Avenue'
 };
 
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const copy = {...employee};
-    copy[key] = value;
-    return copy;
+updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    const newObj = {...employee}
+    newObj[key] = value;
+
+    return newObj;
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    const copy = {...employee};
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
     employee[key] = value;
     return employee;
 }
 
 function deleteFromEmployeeByKey(employee, key) {
-    const copy = {...employee};
-    delete copy[key];
-    return copy;
+    const newObj = {...employee}
+    newObj[key] = employee[key];
+    delete newObj[key];
+    return newObj;
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
     delete employee[key];
     return employee;
 }
+
